8213eda3477631504528a052460620e53e755bd8
ABDULRAHMANYAQOUP <83212579+abdulrahmanyaqoup@users.noreply.github.com>
2021-06-10 12:17:01 +0300

    Update handle_creds.py

--- helpers/handle_creds.py
+++ helpers/handle_creds.py
@@ -1,5 +1,5 @@
 def load_correct_creds(creds, TESTNET):
     if TESTNET:
         return creds['test']['access_key'], creds['prod']['secret_key']
-    else:
+    else
         return creds['prod']['access_key'], creds['prod']['secret_key']
